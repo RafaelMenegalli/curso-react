8ba5d95b833ddc135e65322a3da0efa44f16dd37
Rafael Menegalli <rafaelmenegalli@gmail.com>
2024-08-08 19:26:51 -0300

    Salvando alterações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+// import ShowName from "./components/ShowName";
 
 function App() {
+
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [email, setEmail] = useState("")
+
+  const [users, setUsers] = useState([])
+
+  function handleFormSubmit(event) {
+    event.preventDefault()
+
+    const data = {
+      nome: name,
+      idade: age,
+      email: email
+    }
+
+    const teste = [...users, data]
+
+
+
+    setUsers(teste)
+
+    setName("")
+    setAge("")
+    setEmail("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <label>Nome:</label>
+        <input placeholder="Digite seu nome aqui..." value={name} onChange={(e) => setName(e.target.value)} /> <br />
+
+        <label>Idade:</label>
+        <input placeholder="Digite sua idade aqui..." value={age} onChange={(e) => setAge(e.target.value)} /> <br />
+
+        <label>Email:</label>
+        <input placeholder="Digite seu email aqui..." value={email} onChange={(e) => setEmail(e.target.value)} /> <br /> <br />
+
+        <button type="submit" >Registrar</button>
+      </form>
+
+      <br />
+      <br />
+
+      {users.map((item, index) => (
+        <div key={index}>
+          <span>Nome: {item.nome}</span> <br/>
+          <span>Idade: {item.idade}</span> <br/>
+          <span>Email: {item.email}</span> <br/>
+          <span>------------------------------------------------</span>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/components/ShowName.js
@@ -0,0 +1,6 @@
+
+export default function ShowName({ nome, age }) {
+    return (
+        <span>Seja muito bem vindo {nome}, espero que esteja bem! - Idade {age}</span>
+    )
+}
